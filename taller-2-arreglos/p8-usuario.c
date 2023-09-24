dbaab5e285dd06afe0c13aa5f4d6a56e53907b7d
Daniel Henao <102403567+Danny552@users.noreply.github.com>
2023-09-23 19:37:05 -0500

    punto 8 INGRESADO

---
+++ taller-2-arreglos/p8-usuario.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    int i = 1;
+    printf("Ingrese el tamaño del primer array: ");
+    scanf("%d", &n);
+    int array1[n];
+    printf("Ingrese el primer array:\n");
+    for (i = 1; i <= n; i++)
+    {
+        printf("Ingrese el %d dato: ", i);
+        scanf("%d", &array1[i]);
+    }
+    printf("Ingrese el tamaño del segundo array: ");
+    scanf("%d", &m);
+    int array2[m];
+    printf("Ingrese el segundo array:\n");
+    for (i = 1; i <= m; i++)
+    {
+        printf("Ingrese el %d dato: ", i);
+        scanf("%d", &array2[i]);
+    }
+    i = 1;
+    int z = n + m;
+    int array1_2[z];
+    for (i = 1; i <= n; i++)
+    {
+        array1_2[i] = array1[i];
+    }
+    for (i = 1; i <= n; i++)
+    {
+        array1_2[n + i] = array2[i];
+    }
+
+    for (i = 1; i <= z; i++)
+    {
+        printf("%d ", array1_2[i]);
+    }
+}
